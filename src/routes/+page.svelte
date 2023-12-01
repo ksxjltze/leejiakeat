b5e8e924d83f6f2caa52b4df8bef019bfbde6402
Lee Jia Keat <leejiakeat@gmail.com>
2023-12-01 12:01:43 +0800

    hahae

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,54 +10,19 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+	<h1>Lee Jia Keat</h1>
+</section>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h1 class="text-3xl font-bold underline">
-		Darrick is amazing
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+<section class="home-content">
+	<p>Welcome to my website.</p>
+	<p>My name is Lee Jia Keat, a software engineer based in Singapore.</p>
 </section>
 
 <style lang="postcss">
-	section {
+	.home-content {
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
 		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+		justify-content: center;
 	}
 </style>

b1c5e8b0b8c61e4fddf232f5169ce5b5104d705f
Lee Jia Keat <leejiakeat@gmail.com>
2023-12-01 17:54:54 +0800

    a

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,3 +1,19 @@
-<section>
-    <h1>HELLO DARRICK</h1>
+<script>
+      import "/src/app.css";
+</script>
+
+<section class="flex flex-col gap-4">
+    <h1>Projects</h1>
+    <section class="flex items-center justify-center">
+        <p class="m-auto">You can find my old portfolio website here: <a href="ksxjltze.github.io">ksxjltze.github.io</a></p>
+    </section>
+    <section class="flex items-center justify-center">
+        <h2>Web</h2>
+    </section>
+    <section class="flex items-center justify-center">
+        <h2>Games</h2>
+    </section>
 </section>
+
+<style lang="postcss">
+</style>

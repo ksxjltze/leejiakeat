e2f0f7639af42c79810069e74a97de4b6e6f1ae2
Lee Jia Keat <leejiakeat@gmail.com>
2023-12-10 11:19:56 +0800

    change github

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -32,7 +32,7 @@
 	</nav>
 
 	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
+		<a href="https://github.com/ksxjltze">
 			<img src={github} alt="GitHub" />
 		</a>
 	</div>
--- src/routes/projects/+page.js
+++ src/routes/projects/+page.js
@@ -1,3 +1,5 @@
+export const prerender = true;
+
 export const load = async ({ fetch }) => {
 	const response = await fetch(`/api/projects`);
 	const posts = await response.json();

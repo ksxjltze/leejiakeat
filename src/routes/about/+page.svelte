5dd251d6cedbd13a3e52fa723268879df911b80d
Lee Jia Keat <leejiakeat@gmail.com>
2023-12-24 22:28:13 +0800

    about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,9 +4,17 @@
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
+	<h1>About Me</h1>
 
 	<p>
-		Yes.
+		Hello there. My name is Jia Keat, I’m a Senior at the DigiPen Institute of Technology majoring
+		in Computer Science in Real-Time Interactive Simulation. I’ve been building software for years, including games, game engines, web apps and mobile apps. 
+	</p>
+	<p>
+		Recently, I've been delving into the realm of full-stack development, and I hope to combine my knowledge of game development with web development to create
+		immersive web experiences.
+	</p>
+	<p>
+		I love technology, video games, and building software, as well as an interest in artificial intelligence and virtual reality.
 	</p>
 </div>

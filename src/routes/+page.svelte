cd05838d227c13e33f443db18cf6afafc76d3985
Lee Jia Keat <leejiakeat@gmail.com>
2023-12-25 00:33:02 +0800

    update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,8 +13,34 @@
 </section>
 
 <section class="home-content">
-	<p>Welcome to my website.</p>
-	<p>My name is Lee Jia Keat, a software engineer based in Singapore.</p>
+	<section style="line-height: normal; text-align: center; margin-bottom: 1rem;">
+		<p>Welcome to my website.</p>
+		<p>My name is Lee Jia Keat, a software engineer based in Singapore.</p>
+	</section>
+	
+	<h2 style="font-size: larger; margin-bottom: 2rem;">Click <a style="text-decoration: underline" href="/projects">here</a> to see my work</h2>
+
+	<p>Find me on:</p>
+	<section>
+		<a
+			href="
+		https://www.linkedin.com/in/leejiakeat/
+		"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			<img
+				src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white"
+				alt="Lee Jia Keat's LinkedIn Profile"
+			/>
+		</a>
+		<a href="https://github.com/ksxjltze" target="_blank">
+			<img
+				src="https://img.shields.io/badge/GitHub-333333?style=for-the-badge&logo=github&logoColor=white"
+				alt="Lee Jia Keat's GitHub Profile"
+			/></a
+		>
+	</section>
 </section>
 
 <style lang="postcss">

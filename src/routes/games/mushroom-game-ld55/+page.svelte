00f7b8818e6b757017a4458c429ed90fe2d396ad
Lee Jia Keat <leejiakeat@gmail.com>
2024-04-17 13:06:09 +0800

    instructions for mushroom game

--- src/routes/games/mushroom-game-ld55/+page.svelte
+++ src/routes/games/mushroom-game-ld55/+page.svelte
@@ -3,6 +3,10 @@
 	let game_height = 720;
 </script>
 
+<div class="project-link">
+	<a href="/projects/MushroomGameLD55"><h1>Mushroom Game LD55</h1></a>
+</div>
+
 <section class="game-embed" style='--game-width:{game_width}px; --game-height:{game_height}px'>
 	<iframe
 		title="Mushroom Game LD55"
@@ -14,6 +18,17 @@
 	/>
 </section>
 
+<div class="game-controls">
+	<h2 class="controls-header">How to play</h2>
+	<ol>
+		<li>Click to spawn mushrooms.</li>
+		<li>Buy upgrades for mushrooms.</li>
+		<li>Accumulate enough spores to summon big boi.</li>
+		<li>Defeat enemy hero.</li>
+		<li>Profit?</li>
+	</ol>
+</div>
+
 <style>
 	.game-embed {
 		height: var(--game-height);
@@ -23,5 +38,18 @@
 		position: absolute;
 		left: calc((var(--game-width) / 4));
 		margin-top: 1rem;
+	}
+
+	.game-controls {
+		margin-block-start: 2rem;
+	}
+
+	.controls-header {
+		font-size: x-large;
+		text-decoration: underline;
+	}
+	
+	li {
+		line-height: 1.5rem;
 	}
 </style>
